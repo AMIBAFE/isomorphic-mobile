1a62a0cdb6599e078de119e710fd1e9c8cfc06b3
xuyongtao <xuyongtao2009@gmail.com>
2017-06-14 17:42:18 +0800

    no message

--- server/app.js
+++ server/app.js
@@ -16,6 +16,7 @@ if (process.env.NODE_ENV === 'development') {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '../assets')));
 app.use(express.static(path.join(__dirname, '../dist')));
 
 app.all('*', (req, res, next) => {
