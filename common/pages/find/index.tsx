caaa9cbb0b1726fcce4e20fecc1442088e86bfe7
yangli <121029539@qq.com>
2017-06-19 11:42:11 +0800

    发现页测试

--- common/pages/find/index.tsx
+++ common/pages/find/index.tsx
@@ -3,14 +3,14 @@ import './index.less';
 import * as React from 'react';
 import { render } from 'react-dom';
 
-class Find extends React.Component<any, any> {
+export default class Find extends React.Component<any, any> {
     constructor(props: any, context: any) {
         super(props, context);
     }
     render() {
         return (
            <div id="find">
-            <h4>发现页测试 这里会引入搜索条</h4>
+            <p>发现页测试 这里会引入搜索条</p>
            </div>
         );
     }
