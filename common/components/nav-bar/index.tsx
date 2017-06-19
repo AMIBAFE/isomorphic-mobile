f1c3673591292acf775e985ad616995493f0e1bd
yangli <121029539@qq.com>
2017-06-19 10:44:51 +0800

    nav-bar

--- common/components/nav-bar/index.tsx
+++ common/components/nav-bar/index.tsx
@@ -10,7 +10,37 @@ export default class NavBar extends React.Component<any, any> {
 
     render() {
         return (
-            <div id="nav-bar">导航测试</div>
+            <div id="nav-bar" className="nav-bar">
+                <a className="nav-item">
+                    <i className="nav-icon icon-main">
+                    </i>
+                    <span className="nav-label">
+                        主页
+                    </span>
+                </a>
+                <a className="nav-item">
+                    <i className="nav-icon icon-find">
+                    </i>
+                    <span className="nav-label">
+                        发现
+                    </span>
+                </a>
+                <a className="nav-item">
+                    <i className="nav-icon icon-courses">
+                    </i>
+                    <span className="nav-label">
+                        科目
+                    </span>
+                </a>
+                <a className="nav-item">
+                    <i className="nav-icon icon-my">
+                    </i>
+                    <span className="nav-label">
+                        我的
+                    </span>
+                </a>
+            </div>
+
         )
     }
 }
