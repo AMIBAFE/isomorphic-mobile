c144209321d42c841804162da783eeb6fc2599a2
yangli <121029539@qq.com>
2017-06-19 13:23:23 +0800

    nav引入路由跳转

--- common/components/nav-bar/index.tsx
+++ common/components/nav-bar/index.tsx
@@ -2,6 +2,7 @@ import "./index.less";
 
 import * as React from "react";
 import { render } from "react-dom";
+import { Link } from 'react-router-dom';
 export default class NavBar extends React.Component<any, any> {
 
     constructor(props: any, context: any) {
@@ -10,38 +11,44 @@ export default class NavBar extends React.Component<any, any> {
 
     render() {
         return (
-
             <div id="nav-bar" className="nav-bar">
-                <a className="nav-item">
+                <div className="nav-item">
+                    <Link to="/">
                     <i className="nav-icon icon-main">
                     </i>
                     <span className="nav-label">
                         主页
                     </span>
-                </a>
-                <a className="nav-item">
-                    <i className="nav-icon icon-find">
-                    </i>
-                    <span className="nav-label">
-                        发现
-                    </span>
-                </a>
-                <a className="nav-item">
-                    <i className="nav-icon icon-courses">
-                    </i>
-                    <span className="nav-label">
-                        科目
-                    </span>
-                </a>
-                <a className="nav-item">
-                    <i className="nav-icon icon-my">
-                    </i>
-                    <span className="nav-label">
-                        我的
-                    </span>
-                </a>
+                    </Link>
+                </div>
+                <div className="nav-item">
+                    <Link to="/find">
+                        <i className="nav-icon icon-find">
+                        </i>
+                        <span className="nav-label">
+                            发现
+                        </span>
+                    </Link>
+                </div>
+                <div className="nav-item">
+                    <Link to={`/cats`}>
+                        <i className="nav-icon icon-courses">
+                        </i>
+                        <span className="nav-label">
+                            科目
+                        </span>
+                    </Link>
+                </div>
+                <div className="nav-item">
+                    <Link to={`/user`}>
+                        <i className="nav-icon icon-my">
+                        </i>
+                        <span className="nav-label">
+                            我的
+                        </span>
+                    </Link>
+                </div>
             </div>
-
         )
     }
 }
