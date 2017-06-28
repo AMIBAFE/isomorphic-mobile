b0d7f5c2d0a618df83a341b0163105de63880282
yangli <121029539@qq.com>
2017-06-28 18:29:10 +0800

    reduces里面传参写法错误 所以报错

--- common/reducers/cat.ts
+++ common/reducers/cat.ts
@@ -5,7 +5,7 @@ import { HotSearchCatBasic } from "../interfaces/cat";
 import { ADD_HOT_SEARCH_CATS } from "../actions/cats";
 
 export function hotSearchReducer(
-    state: HotSearchCatBasic,
+    state: any = {},
     action: {
         type: string;
         hotSearchCats: HotSearchCatBasic;
