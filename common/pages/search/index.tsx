dcc0eca65578a8ff3560f52d0f1dd8631fe541f7
yangli <121029539@qq.com>
2017-07-07 10:43:20 +0800

    return-btn

--- common/pages/search/index.tsx
+++ common/pages/search/index.tsx
@@ -3,6 +3,8 @@ import "./index.less";
 import * as React from "react";
 import { render } from "react-dom";
 
+import AutoComplete from "../../components/autocomplete/index";
+import ReturnBtn from "../../components/return-btn/index";
 export default class Search extends React.Component<any, any> {
     constructor(props: any, context: any) {
         super(props, context);
@@ -10,6 +12,10 @@ export default class Search extends React.Component<any, any> {
     render() {
         return (
             <div id="search-wrapper">
+                <div className="search-bar">
+                    <ReturnBtn history={this.props.history} />
+                    <AutoComplete />
+                </div>
                 <div>这是搜索页这是header</div>
             </div>
         );
