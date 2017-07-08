cd7acd19ea4fdfcd04d58d600805cf1bfc9b82ae
yangli <121029539@qq.com>
2017-07-08 09:15:12 +0800

    点击按钮 发送请求

--- common/components/search-bar/index.tsx
+++ common/components/search-bar/index.tsx
@@ -4,81 +4,109 @@ import * as React from "react";
 
 import { render } from "react-dom";
 import { Link } from "react-router-dom";
+import { Dispatch } from "redux";
+import { connect } from "react-redux";
+
 import { number, string, array, bool, func } from "prop-types";
-import { TeacherBasic } from '../../interfaces/teacher';
+import { TeacherBasic } from "../../interfaces/teacher";
 import { getSuggestion } from "../../actions/search";
 
 interface SearchProps {
-  searchLists?: TeacherBasic[];
+    searchLists?: TeacherBasic[];
 }
 interface SearchState {
-  value?: string;
+    value?: string;
+    suggestions?: any;
 }
 export default class Search extends React.Component<SearchProps, SearchState> {
-  constructor(props: SearchProps) {
-    super(props);
-    this.state = {
-      value: ""
-    };
-  }
-  onClick(event: any) {
-    event.preventDefault();
-    console.log(this.state.value);
-  }
-  onChange(event: any) {
-    let value = event.target.value;
+    timer: number;
+    constructor(props: SearchProps) {
+        super(props);
+        this.state = {
+            value: "",
+            suggestions: []
+        };
+    }
+    normalizeInput() {
+        return this.state.value.toLowerCase().trim();
+    }
+    search() {
+        if (!this.state.value) return;
+        const keyword = this.normalizeInput();
+        const inputNode: any = this.refs["input"];
+        // 点击按钮 发送请求
+        /* let { dispatch }: { dispatch: Dispatch<any> } = this.props;*/
 
-    this.setState(state => {
-      state.value = value;
-      return state;
-    });
-  }
+        /* const fetchData = (
+            { dispatch }: { dispatch: Dispatch<any> },
+            params: { keyword: string }
+        ) => dispatch(getSuggestion({ ...params }));*/
+    }
 
-  render() {
-    return (
-      <div className="search-bar-wrapper col-l">
-        <div className="search-box-wrapper">
-          <div >
-            <div className="type-wrapper">
-              <div className="type-header dropdown">
-                <a href="javascript:;" className="dropdown-header">
-                  课程
-                </a>
-                <div className="dropdown-panel">
-                  <a className="dropdown-item">
-                    <i className="dropdown-icon course" />
-                    <span className="dropdown-label">
-                      课程
-                    </span>
-                  </a>
-                  <a className="dropdown-item">
-                    <i className="dropdown-icon studio" />
-                    <span className="dropdown-label">
-                      机构
-                    </span>
-                  </a>
-                  <a className="dropdown-item">
-                    <i className="dropdown-icon teacher" />
-                    <span className="dropdown-label">
-                      老师
-                    </span>
-                  </a>
+    onSearch(event: any) {
+        event.preventDefault();
+        this.search();
+    }
+    onChange(event: any) {
+        let value = event.target.value;
+
+        this.setState(state => {
+            state.value = value;
+            return state;
+        });
+    }
+
+    render() {
+        return (
+            <div className="search-bar-wrapper col-l">
+                <div className="search-box-wrapper">
+                    <div>
+                        <div className="type-wrapper">
+                            <div className="type-header dropdown">
+                                <a
+                                    href="javascript:;"
+                                    className="dropdown-header"
+                                >
+                                    课程
+                                </a>
+                                <div className="dropdown-panel">
+                                    <a className="dropdown-item">
+                                        <i className="dropdown-icon course" />
+                                        <span className="dropdown-label">
+                                            课程
+                                        </span>
+                                    </a>
+                                    <a className="dropdown-item">
+                                        <i className="dropdown-icon studio" />
+                                        <span className="dropdown-label">
+                                            机构
+                                        </span>
+                                    </a>
+                                    <a className="dropdown-item">
+                                        <i className="dropdown-icon teacher" />
+                                        <span className="dropdown-label">
+                                            老师
+                                        </span>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <input
+                            ref="input"
+                            type="text"
+                            value={this.state.value}
+                            onChange={this.onChange.bind(this)}
+                            placeholder="请输入您要找的内容"
+                        />
+                        <a
+                            onClick={this.onSearch.bind(this)}
+                            href="javascript:;"
+                        >
+                            <span className="text">确定</span>
+                        </a>
+                    </div>
                 </div>
-              </div>
             </div>
-            <input
-              ref="input"
-              type="text"
-              value={this.state.value}
-              onChange={this.onChange.bind(this)}
-              placeholder="请输入您要找的内容"
-            />
-            <a onClick={this.onClick.bind(this)} href="javascript:;">
-              <span className="text">确定</span>
-            </a>
-          </div>
-        </div>
-      </div>
-    );
-  }
+        );
+    }
 }
--- common/pages/teacher/index.tsx
+++ common/pages/teacher/index.tsx
@@ -1,17 +1,17 @@
-import './index.less';
+import "./index.less";
 
-import * as React from 'react';
-import { render } from 'react-dom';
-import { connect } from 'react-redux';
-import { Dispatch } from 'redux';
-import { number, string, array, object } from 'prop-types';
+import * as React from "react";
+import { render } from "react-dom";
+import { connect } from "react-redux";
+import { Dispatch } from "redux";
+import { number, string, array, object } from "prop-types";
 
-import { defaultAvatar } from '../../configs/vars';
-import { fetchTeacherDetail } from '../../actions/teacher';
-import { TeacherBasic } from '../../interfaces/teacher';
+import { defaultAvatar } from "../../configs/vars";
+import { fetchTeacherDetail } from "../../actions/teacher";
+import { TeacherBasic } from "../../interfaces/teacher";
 
-import CourseCard from '../../components/course-card';
-import fetch from '../../../client/fetch';
+import CourseCard from "../../components/course-card";
+import fetch from "../../../client/fetch";
 /*
 function mapStateToProps(state: any) {
     const teacherResponse: TeacherBasic = state.teacher;
@@ -28,50 +28,85 @@ class Teacher extends React.Component<TeacherBasic, any> {
         return (
             <div id="teacher-home">
                 <section id="info-part">
-                    <img src={this.props.avatar || defaultAvatar} alt={this.props.name} />
+                    <img
+                        src={this.props.avatar || defaultAvatar}
+                        alt={this.props.name}
+                    />
                     <div>
-                        <h1><strong>{this.props.name}</strong>{this.props.labels}</h1>
-                        <p>{this.props.shortIntro}</p>
+                        <h1>
+                            <strong>
+                                {this.props.name}
+                            </strong>
+                            {this.props.labels}
+                        </h1>
+                        <p>
+                            {this.props.shortIntro}
+                        </p>
                         <ul>
-                            <li><span className="label">全部课程：</span><span className="courses">{this.props.courses && this.props.courses.map((course, index) => {
-                                return (
-                                    <span key={index}>{course.cats[course.cats.length - 1]}</span>
-                                )
-                            })}</span></li>
-                            <li>教学年龄：<strong>{this.props.teachingAge}年</strong></li>
+                            <li>
+                                <span className="label">全部课程：</span>
+                                <span className="courses">
+                                    {this.props.courses &&
+                                        this.props.courses.map(
+                                            (course, index) => {
+                                                return (
+                                                    <span key={index}>
+                                                        {
+                                                            course.cats[
+                                                                course.cats
+                                                                    .length - 1
+                                                            ]
+                                                        }
+                                                    </span>
+                                                );
+                                            }
+                                        )}
+                                </span>
+                            </li>
+                            <li>
+                                教学年龄：<strong>{this.props.teachingAge}年</strong>
+                            </li>
                         </ul>
                     </div>
                 </section>
                 <section id="course-part" className="main-part">
-                    <h2 className="main-title"><strong>全部课程</strong>ALL COURSE</h2>
+                    <h2 className="main-title">
+                        <strong>全部课程</strong>ALL COURSE
+                    </h2>
                     <ul>
-                        {this.props.courses && this.props.courses.map((course, index) => {
-                            return (
-                                <CourseCard key={course.id} {...course} />
-                            )
-                        })}
+                        {this.props.courses &&
+                            this.props.courses.map((course, index) => {
+                                return (
+                                    <CourseCard key={course.id} {...course} />
+                                );
+                            })}
                     </ul>
                 </section>
                 <section id="intro-part" className="main-part">
-                    <h2 className="main-title"><strong>教师简介</strong>TEACHER INTRODUCTION</h2>
-                    <div>{this.props.intro}</div>
+                    <h2 className="main-title">
+                        <strong>教师简介</strong>TEACHER INTRODUCTION
+                    </h2>
+                    <div>
+                        {this.props.intro}
+                    </div>
                 </section>
             </div>
-        )
+        );
     }
 }
 
+const fetchData = (
+    { dispatch }: { dispatch: Dispatch<any> },
+    params: { tid: number }
+) => dispatch(fetchTeacherDetail({ ...params }));
 
-
-const fetchData = ({ dispatch }: { dispatch: Dispatch<any> }, params: { tid: number }) =>
-    dispatch(fetchTeacherDetail({ ...params }));
+//将state.teacherResponse绑定到props的counter
 
 function mapStateToProps(state: any) {
     const teacherResponse: TeacherBasic = state.teacher;
 
     return { ...teacherResponse };
 }
-
 
 (Teacher as any).propTypes = {
     id: number,
@@ -81,9 +116,8 @@ function mapStateToProps(state: any) {
     avatar: string,
     courses: array,
     shortIntro: string,
-    intro: string,
-}
-
+    intro: string
+};
 
 const ConnectedComponent = connect(mapStateToProps)(Teacher as any);
 
@@ -93,5 +127,4 @@ const fetchData = ({ dispatch }: { dispatch: Dispatch<any> }, params: { tid: num
 */
 export default fetch(fetchData)(ConnectedComponent);
 
- //  export default fetch(fetchData)(Teacher);
-
+//  export default fetch(fetchData)(Teacher);
